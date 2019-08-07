17b48e294c883f5347b8e12487601e685757ea2e
Markin Igor <markin.io210@gmail.com>
2019-08-07 12:04:38 +0500

    Implement recursive binary search.

--- binary_search.go
+++ binary_search.go
@@ -29,12 +29,37 @@ func binarySearch(array []int, value int) int {
 	return -1
 }
 
+func binarySearchRecursive(array []int, value int) int {
+	if len(array) == 0 || array[0] > value || array[len(array)-1] < value {
+		return -1
+	}
+
+	low := 0
+	high := len(array) - 1
+
+	mid := (low + high) / 2
+
+	guess := array[mid]
+
+	if guess == value {
+		return mid
+	} else if guess > value {
+		return binarySearchRecursive(append([]int{}, array[0:mid]...), value)
+	} else if guess < value {
+		return mid + 1 + binarySearchRecursive(append([]int{}, array[mid+1:]...), value)
+	}
+
+	return -1
+}
+
 func main() {
 	var data = ReadPipeInput()
 
 	var toFind, _ = strconv.Atoi(string(os.Args[1]))
-	log.Printf("Search for %v", toFind)
+	log.Printf("Search for %d", toFind)
 
-	index := binarySearch(data, toFind)
-	log.Printf("Index %d", index)
+	searchIndex := binarySearch(data, toFind)
+	recursiveSearchIndex := binarySearchRecursive(data, toFind)
+
+	log.Printf("Index is %d and %d", searchIndex, recursiveSearchIndex)
 }
